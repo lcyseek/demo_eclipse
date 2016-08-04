fb18b5c53862e6f202c0fa91f8634f23caed4ff3
luchunyang <lcyseek@gmail>
2016-08-04 10:19:52 +0800

    http工程

---
+++ SimpleHttp/src/com/seek/http/SimpleDemo.java
@@ -0,0 +1,22 @@
+package com.seek.http;
+
+public class SimpleDemo {
+
+	private static String BAIDU = "http://www.baidu.com/";
+	private static String SINA = "http://www.sina.com.cn/";
+	private static String BIYING="http://cn.bing.com/";
+	private static String url = "http://www.google.com";
+	private static String LIUYAN = "http://gb.cri.cn/mmsource/images/2011/12/14/24/5004665712453389868.jpg";
+	
+	public static void main(String[] args) throws Exception {
+
+//		HttpUtil.get(url);
+//		HttpUtil.getImage(LIUYAN);
+//		HttpUtil.getMp3Info("心太软");
+		
+//		HttpUtil.post(url);
+	}
+	
+
+
+}
